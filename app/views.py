80fc7b6b86e32ee7ebca4dbcc9bd337f137cb982
Nursultan Berdiev <nursultanberdiev1994@gmail.com>
2022-07-16 18:10:49 +0600

    For Students

---
+++ app/views.py
@@ -0,0 +1,30 @@
+# post functions
+
+def index():
+    pass
+
+
+def customer_create():
+    pass
+
+
+def customer_detail(customer_id):
+    pass
+
+
+def customer_delete(customer_id):
+    pass
+
+
+def customer_update(customer_id):
+    pass
+
+
+# user functions
+
+def register():
+    pass
+
+
+def login():
+    pass
